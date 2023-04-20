09e0c9c8ee556c6b329f449c95f3c5e2cbc1d8c8
Lucas Neris <lucasnerism@gmail.com>
2023-04-20 18:23:34 -0300

    Transaction component and .env adjustments

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,22 @@
+import { Link } from "react-router-dom";
 import { ContainerTransaction } from "./Styled.js";
 
 export default function Transaction({date, description, value, type}){
   
+  function deleteEntry(){}
+
   return(
     <ContainerTransaction>
-      <div><p style={{color: "#C6C6C6", marginRight: "10px"}}>{date}</p><p> {description}</p></div>
-      <p style={{color: type === "in"? "#03AC00" : "#C70000"}} >{value}</p>
+      <div>
+        <p style={{color: "#C6C6C6", marginRight: "10px"}}>{date}</p>
+        <p onClick={deleteEntry} style={{cursor: "pointer"}}> {description}</p>
+      </div>
+      <div>
+        <p style={{color: type === "in"? "#03AC00" : "#C70000", marginRight: "10px"}} >{value}</p>
+        <Link>
+        <p style={{color: "#C6C6C6", cursor: "pointer"}}>x</p>
+        </Link>
+      </div>
     </ContainerTransaction>
   )
 }
